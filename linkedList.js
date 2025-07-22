c0e4cca698da1cb563dc9a144c40453338da2b74
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-07-22 21:33:59 +0200

    Add basic js files and classes constructors

---
+++ linkedList.js
@@ -0,0 +1,12 @@
+export default class LinkedList {
+    constructor(){
+        list = []
+    }
+}
+
+class Node {
+    constructor(){
+        this.value = null
+        this.nextNode = null
+    }
+}
