c3ed879aa67b7c000204019623194038d4489029
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-07-22 21:43:59 +0200

    Add append and prepend functions

--- linkedList.js
+++ linkedList.js
@@ -1,12 +1,39 @@
 export default class LinkedList {
     constructor(){
-        list = []
+        this.list = []
+        head = null
+        tail = null
+        size = 0
+    }
+
+    append(value){
+        const node = new Node(value)
+        this.list.push(node)
+        if(this.size === 0){
+            head = node
+        }
+        tail.nextNode = node
+        tail = node
+
+        this.size++
+    }
+
+    prepend(value){
+        const node = new Node(value)
+        this.list.push(node)
+        if(this.size === 0){
+            tail = node
+        }
+        node.nextNode = head
+        head = node
+
+        this.size++
     }
 }
 
 class Node {
-    constructor(){
-        this.value = null
+    constructor(value){
+        this.value = value
         this.nextNode = null
     }
 }
